cb5c519af7bbf7443fae5da982f28a8af054420f
Afzal Hossan <afzalhossan2005021@gmail.com>
2024-09-14 14:52:24 +0600

    Converting from dos to unix and then reading input file added.

--- Bash-Scripting/2005021.sh
+++ Bash-Scripting/2005021.sh
@@ -11,3 +11,13 @@ if [ ! -r $2 ]; then
     echo "File is not readable: $2"
     exit 1
 fi
+
+# Convert the input_file to unix format
+dos2unix -q $2
+
+# Create an array to store the lines
+input_file=()
+# Read each line into the array
+while IFS= read -r line; do
+    input_file+=("$line")  # Append each line to the array
+done < "$2"
